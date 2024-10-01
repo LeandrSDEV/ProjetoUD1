e8cd74b082cea70155da1a7c181e35e4c8b5f532
LEANDRO <crazyg4ng@gmail.com>
2024-10-01 17:59:38 -0300

    Injeção de dependencia direto da classe para popular - SeedingService

--- Program.cs
+++ Program.cs
@@ -13,14 +13,22 @@ namespace ProjetoUd1
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            builder.Services.AddScoped<SeedingService>();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.            
                 app.UseHsts();
+            }
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var seedingService = scope.ServiceProvider.GetRequiredService<SeedingService>();
+                seedingService.Seed();
             }
 
             app.UseHttpsRedirection();
